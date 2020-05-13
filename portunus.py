89aaf0a4294702d3d2dbc40414ab5ab5938af169
Maxwell <msf9542@g.rit.edu>
2020-05-13 13:52:16 -0400

    allows use of cd

--- portunus.py
+++ portunus.py
@@ -1,20 +1,25 @@
 import argparse
 import os
-import sys
 
 parser = argparse.ArgumentParser(prog='portunus',
                                  description="creates a backdoor to install on a system to gain persistence")
 connection = parser.add_mutually_exclusive_group()
 connection.add_argument(
-    "--udp", "-u", help="utilizes udp for shell interation")
+    "-u", "--udp", help="utilizes udp for shell interation")
 connection.add_argument(
-    "--tcp", '-t', help="estbalishes tcp connection for shell interation")
+    "-t", '--tcp', help="estbalishes tcp connection for shell interation")
 
+# parser.print_help()
 while True:
     command = input("> ")
     if 'quit' == command or command == 'exit':
         break
     try:
         os.system(command)
+        if command[:3] == 'cd ':
+            try:
+                os.chdir(command[3:])
+            except:
+                pass
     except:
         print("failure executing command")
