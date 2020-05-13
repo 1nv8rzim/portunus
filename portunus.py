f01ca225858d444d1ec6d3e6379cdf7b5e87cfa8
Maxwell <msf9542@g.rit.edu>
2020-05-13 14:00:46 -0400

    functional organization

--- portunus.py
+++ portunus.py
@@ -1,25 +1,38 @@
 import argparse
 import os
 
-parser = argparse.ArgumentParser(prog='portunus',
-                                 description="creates a backdoor to install on a system to gain persistence")
-connection = parser.add_mutually_exclusive_group()
-connection.add_argument(
-    "-u", "--udp", help="utilizes udp for shell interation")
-connection.add_argument(
-    "-t", '--tcp', help="estbalishes tcp connection for shell interation")
 
-# parser.print_help()
-while True:
-    command = input("> ")
-    if 'quit' == command or command == 'exit':
-        break
-    try:
-        os.system(command)
-        if command[:3] == 'cd ':
-            try:
-                os.chdir(command[3:])
-            except:
-                pass
-    except:
-        print("failure executing command")
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="creates a backdoor to install on a system to gain persistence")
+    connection = parser.add_mutually_exclusive_group()
+    connection.add_argument(
+        "-u", "--udp", help="utilizes udp for shell interation")
+    connection.add_argument(
+        "-t", '--tcp', help="estbalishes tcp connection for shell interation")
+    parser.parse_args()
+
+
+def run_shell():
+    while True:
+        command = input("> ")
+        if 'quit' == command or command == 'exit':
+            break
+        try:
+            os.system(command)
+            if command[:3] == 'cd ':
+                try:
+                    os.chdir(command[3:])
+                except:
+                    pass
+        except:
+            print("failure executing command")
+
+
+def main():
+    parse_args()
+    run_shell()
+
+
+if __name__ == "__main__":
+    main()
