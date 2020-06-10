b7a5278e40ef5cc45d6541c4ff2a9c0b4eaacc85
Maxwell <msf9542@g.rit.edu>
2020-06-09 22:01:34 -0400

    first commit on new version of portunus.py

---
+++ portunus.py
@@ -0,0 +1,9 @@
+import argparse
+import paramiko
+
+"""
+portunus makes and managages sshable port on a system in order to gain persistence
+**NOTE** this is for educational purposes only
+author:     1nv8rZim
+language:   python 3.8
+"""
