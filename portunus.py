2ce4b3e35cb283570a15dd14eaf628c03cbe2eb7
Maxwell <msf9542@g.rit.edu>
2020-06-11 23:02:08 -0400

    planning

--- portunus.py
+++ portunus.py
@@ -7,3 +7,24 @@ portunus makes and managages sshable port on a system in order to gain persisten
 author:     1nv8rZim
 language:   python 3.8
 """
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    """
+    needed args
+    ---
+    username
+    password
+    ---
+    ssh-key
+    ---
+    randomization key
+    ---
+    port-range
+    ---
+    replication name
+    ---
+    more to come...
+    """
+    return parser.parse_args()
