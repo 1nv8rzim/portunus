23c34f47e9b2a851db0fbbc712f2b06b48f2e357
Maxwell <msf9542@g.rit.edu>
2020-10-03 19:37:57 -0400

    general skeleton for portunus

---
+++ portunus.py
@@ -0,0 +1,21 @@
+import argparse
+
+
+class reverse_shell:
+    """Creates reverse shell
+    """
+
+    def __init__(self):
+        """Runs main loop of reverse_shell
+        """
+        self.parser = self.parse_args()
+
+    def parse_args(self):
+        """Parses command line arguments into usable parameters
+
+        Returns:
+            argparse.Namespace: parsed command line arguments
+        """
+        parser = argparse.ArgumentParser()
+
+        return parser.parse_args()
